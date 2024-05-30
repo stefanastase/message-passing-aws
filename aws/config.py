6bcbe503a0db703a7de0cdb1e4137f93db4a6b51
stefanastase <ec2-user@ip-172-31-13-188.eu-north-1.compute.internal>
2024-05-30 07:09:46 +0000

    Subscribe queues to SNS topic

--- aws/config.py
+++ aws/config.py
@@ -9,12 +9,39 @@ topic_arn = response['TopicArn']
 sqs_client = boto3.client('sqs')
 response = sqs_client.create_queue(QueueName='small-queue')
 small_queue_url = response['QueueUrl']
+small_queue_attrs = sqs_client.get_queue_attributes(QueueUrl=small_queue_url, AttributeNames=['All'])['Attributes']
+small_queue_arn = small_queue_attrs['QueueArn']
+
+# Subscribe SQS queue to SNS
+sns_client.subscribe(
+    TopicArn=topic_arn,
+    Protocol='sqs',
+    Endpoint=small_queue_arn
+)
 
 response = sqs_client.create_queue(QueueName='large-queue')
 large_queue_url = response['QueueUrl']
+large_queue_attrs = sqs_client.get_queue_attributes(QueueUrl=large_queue_url, AttributeNames=['All'])['Attributes']
+large_queue_arn = large_queue_attrs['QueueArn']
+
+# Subscribe SQS queue to SNS
+sns_client.subscribe(
+    TopicArn=topic_arn,
+    Protocol='sqs',
+    Endpoint=large_queue_arn
+)
 
 response = sqs_client.create_queue(QueueName='metrics-queue')
 metrics_queue_url = response['QueueUrl']
+metrics_queue_attrs = sqs_client.get_queue_attributes(QueueUrl=metrics_queue_url, AttributeNames=['All'])['Attributes']
+metrics_queue_arn = metrics_queue_attrs['QueueArn']
+
+# Subscribe SQS queue to SNS
+sns_client.subscribe(
+    TopicArn=topic_arn,
+    Protocol='sqs',
+    Endpoint=metrics_queue_arn
+)
 
 config = {
     "SNS_TOPIC_ARN": topic_arn,
